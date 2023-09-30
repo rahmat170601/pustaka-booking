df3b50e3afc67f6987b7b34790373a6f21218e2f
rahmat170601 <rahmat1.hidaya@gmail.com>
2023-09-30 23:34:48 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+error_reporting(0);
+class contoh1 extends ci_controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya rahmat hidayat
+        saya tinggal di daerah bekasi utara
+        olahraga yang saya sukai adalah badminton";
+    }
+}
